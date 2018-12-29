f1a38bb149760c1ac1dfcc47234c1386eb304257
Carlos Barreiro Mata <barreymata@gmail.com>
2018-12-29 21:46:41 +0100

    update padding in the ranking table

--- react/src/Containers/Temporada/RankingTable.tsx
+++ react/src/Containers/Temporada/RankingTable.tsx
@@ -2,6 +2,15 @@ import * as React from 'react'
 import { TeamRank } from './TeamRankType'
 import { TableHead, Table, TableRow, TableCell, TableBody } from '@material-ui/core'
 
+const cellStyle: React.CSSProperties = {
+  padding: '5px'
+}
+
+const firstCellStyle = {
+  padding: '5px, 5px, 5px, 10px'
+
+}
+
 type IProps = {
   ranking: TeamRank[]
 }
@@ -11,28 +20,28 @@ export const RankingTable = (props: IProps) => {
   return (<Table>
     <TableHead>
       <TableRow>
-        <TableCell></TableCell>
-        <TableCell>Nombre del equipo</TableCell>
-        <TableCell>Pts</TableCell>
-        <TableCell>PG</TableCell>
-        <TableCell>PE</TableCell>
-        <TableCell>PP</TableCell>
-        <TableCell>GF</TableCell>
-        <TableCell>GC</TableCell>
+        <TableCell style={firstCellStyle}></TableCell>
+        <TableCell style={cellStyle}>Nombre del equipo</TableCell>
+        <TableCell style={cellStyle}>Pts</TableCell>
+        <TableCell style={cellStyle}>PG</TableCell>
+        <TableCell style={cellStyle}>PE</TableCell>
+        <TableCell style={cellStyle}>PP</TableCell>
+        <TableCell style={cellStyle}>GF</TableCell>
+        <TableCell style={cellStyle}>GC</TableCell>
       </TableRow>
     </TableHead>
     <TableBody>
       {props.ranking.map((row, index) => {
         return (
           <TableRow key={'row' + index}>
-            <TableCell>{row.Posicion}</TableCell>
-            <TableCell>{row.Nombre_equipo}</TableCell>
-            <TableCell>{row.Puntos}</TableCell>
-            <TableCell>{row.Partidos_ganados}</TableCell>
-            <TableCell>{row.Partidos_empatados}</TableCell>
-            <TableCell>{row.Partidos_perdidos}</TableCell>
-            <TableCell>{row.Goles_favor}</TableCell>
-            <TableCell>{row.Goles_contra}</TableCell>
+            <TableCell style={firstCellStyle}>{row.Posicion}</TableCell>
+            <TableCell style={cellStyle}>{row.Nombre_equipo}</TableCell>
+            <TableCell style={cellStyle}>{row.Puntos}</TableCell>
+            <TableCell style={cellStyle}>{row.Partidos_ganados}</TableCell>
+            <TableCell style={cellStyle}>{row.Partidos_empatados}</TableCell>
+            <TableCell style={cellStyle}>{row.Partidos_perdidos}</TableCell>
+            <TableCell style={cellStyle}>{row.Goles_favor}</TableCell>
+            <TableCell style={cellStyle}>{row.Goles_contra}</TableCell>
           </TableRow>
         );
       })}
