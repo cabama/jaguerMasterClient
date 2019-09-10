13abcfe51ad14a372f2244d454580f0faaa712bc
cabama <barreymata@gmail.com>
2019-09-10 02:47:54 +0200

    edit index page

--- madridClient/src/Containers/Main/Main.tsx
+++ madridClient/src/Containers/Main/Main.tsx
@@ -2,30 +2,35 @@ import * as React from 'react'
 import { View } from 'Components/View/View'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
-import CardHeader from '@material-ui/core/CardHeader'
 import CardActions from '@material-ui/core/CardActions'
 import Button from '@material-ui/core/Button'
-import { useRouter } from 'Shared/router'
+import Typography from '@material-ui/core/Typography'
 
 export const MainPage = () => {
-  const router = useRouter()
   return <div>
     <View MenuBar={true} SideMenu={true}>
         <Card style={{ margin: '20px 0' }}>
-          <CardHeader title="Juegos Deportivos Municipales"/>
         <CardContent>
-          <p>Web no Oficial de los juegos deportivos de madrid. <br></br>
-          <a href="https://www-2.munimadrid.es/ResultadosCompeticionesDeportivas/competicion.form">
-            Enlace a la página oficial
-          </a>
-          </p>
+        <Typography variant="h6" component="h1" gutterBottom>
+        Juegos Deportivos Municipales de Madrid
+        </Typography>
+
+          <p>Web no Oficial de los juegos deportivos de madrid.</p>
           <p>
           En esta pagina se mostraran las
-          clasifiaciones, resultados y próximos encuentros de los juegos deportivos municipales.
+          clasificaciones, resultados y próximos encuentros de los juegos deportivos municipales.
           </p>
+
+          <p>
+            Los Juegos Deportivos cumplen 40 ediciones con la misma filosofía con
+            la que se crearon: la participación y la deportividad como los principales objetivos,
+            por encima de los grandes resultados.
+          </p>
+
+          <p>Inscripciones del 18 de marzo al 10 de abril de 2020 para los deportes de equipo.</p>
         </CardContent>
-        <CardActions style={{ display: 'flex', flexDirection: 'row-reverse' }}>
-        <Button size="medium" color="primary" onClick={() => router.history.push('/buscar')}>
+        <CardActions style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+        <Button variant="contained" size="medium" color="primary" href="/buscar">
           Busca tu equipo
         </Button>
         </CardActions>
