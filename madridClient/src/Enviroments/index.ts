ac8158a2e54a1b40bffb96f31f374e4f86b66156
Carlos Barreiro Mata <barreymata@gmail.com>
2020-01-16 02:13:08 +0100

    Update index.ts

--- madridClient/src/Enviroments/index.ts
+++ madridClient/src/Enviroments/index.ts
@@ -13,4 +13,4 @@ export function getUrlsEnviroment(): IUrlsEnv {
 
 export const jagerServiceBaseUrl = false
   ? 'http://localhost:2525'
-  : 'http://https://juegosdeportivosmunicipales.es/api'
+  : 'https://https://juegosdeportivosmunicipales.es/api'
